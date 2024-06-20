33f48970e284dcd19706fd44d2665d4a38206e84
alimomennasab <alimomennasab3@gmail.com>
2024-06-19 23:02:30 -0700

    reworked scraping, now only get the top 100 prospects, and sort them by position

--- prospect_scraping.py
+++ prospect_scraping.py
@@ -4,29 +4,32 @@ from selenium.webdriver.common.by import By
 driver = Safari()
 
 # make a dictionary with positions and a list of prospects for each position
-positions = ["QB", "RB", "WR", "TE", "OT", "IOL", "DL", "EDGE", "LB", "CB", "S", "K", "P", "LS"]
+positions = ["QB", "RB", "WR", "TE", "OT", "IOL", "DL", "EDGE", "LB", "CB", "S"] #"K", "P", "LS" likely won't be in top 100
 prospects = {position: [] for position in positions}
 
-
+# go to the URL for every position
 for position in positions:
-    print("Position: ", position)
     url = 'https://www.nflmockdraftdatabase.com/big-boards/2025/consensus-big-board-2025?pos=' + position
     driver.get(url)
+    print("Position: ", position)
+
     players = driver.find_elements(By.XPATH, '//div[@class="player-name player-name-smaller"]')
+    rankings = driver.find_elements(By.XPATH, '//div[@class="pick-number with-subtitle"]')
+    print(f"Players: {len(players)} , Rankings: {len(rankings)}")
+
+    num_players = min(len(players), len(rankings))
     
-    players_list = []
-     # get the top 10 players per position, 
-     # or the max number of players under unpopular positions (K/LS)
-    for p in range(min(len(players), 10)):
+    # check each position group for top 100 ranked players,
+    for p in range(num_players):
         player_name = players[p].text
-        players_list.append(player_name)
-        # print("Added:", player_name)
-        prospects[position] = players_list
+        player_ranking = int(rankings[p].text)
+        # if the current prospect has a higher rank than 100, skip the rest of the position group
+        if (player_ranking > 100):
+            print("Skipping rest of ", position)
+            break
+        prospects[position].append((player_ranking, player_name))
+        print(f"Added {player_name} (#{player_ranking})")
 
 driver.close()
-print("Driver closed")
+print("Driver closed, scraping complete")
 
-for position, players in prospects.items():
-    print(f"\nTop 10 {position} prospects:")
-    for player in players:
-        print(player)
