cc5bb02192d92b335998b570ea49549c63946a18
alimomennasab <alimomennasab3@gmail.com>
2024-07-28 16:55:25 -0700

    added prospect component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Prospect from "../components/Prospect"
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    return <Prospect pickNumber={1} prospectName={"Luther Burden"} position={"WR"} logoURL={"images/mizzou.png"}></Prospect>
   }
---
+++ src/components/Prospect.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface ProspectProps {
+  pickNumber: number;
+  prospectName: string;
+  position: string;
+  logoURL: string;
+}
+
+const Prospect: React.FC<ProspectProps> = ({ pickNumber, prospectName, position, logoURL }) => {
+  return (
+    <div className="bg-white w-80 h-16 border-black rounded border flex flex-row justify-between items-center">
+      <div className="bg-white w-8 h-12 border-white flex rounded border m-1 items-center justify-start text-lg font-bold">
+        #{pickNumber}
+      </div>
+      <div className='bg-white w-56 h-12 border-white flex rounded border items-center justify-start text-lg'>
+        {prospectName} ({position})
+      </div>
+      <div className='bg-white w-12 h-12 border-black border m-1 rounded-full flex items-center justify-center'>
+        <img src={logoURL} alt="Img" className="w-full h-full object-cover rounded-full" />
+      </div>
+    </div>
+  );
+};
+
+export default Prospect;
