a122684a4f013b9fd250932ceaffa3cdd318e732
alimomennasab <alimomennasab3@gmail.com>
2024-07-30 10:43:25 -0700

    updated main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,29 +41,34 @@ export default function Page() {
 
   return (
     <div className='flex justify-center items-center h-screen flex-col'>
-      <div className='md:max-w-5xl flex flex-col items-center'>
-        <div className='font-bold p-2 border border-white text-4xl md:max-w-5xl'>
+      <div className='md:max-w-5xl flex flex-col items-center w-full'>
+        <div className='font-bold p-2 border border-white text-4xl w-full text-center'>
           NFL 2025 Mock Draft
         </div>
-
-        <div className='italic p-2 border border-white text-xs text-gray-700'>
+        <div className='italic pb-2 border border-white text-xs text-gray-700 w-full text-center'>
           Modify the draft order by dragging and dropping each team
         </div>
 
-        <div className='md:max-w-5xl justify-center border border-gray-300 items-center p-4 rounded-lg'>
+        <div className='md:max-w-5xl justify-center border border-gray-300 items-center p-4 rounded-lg w-full'>
           <DraftOrderGrid draftCapital={draftCapital} setDraftCapital={setDraftCapital} />
         </div>
 
-        <div className='flex justify-center items-center'>
-          <ResetDraftButton onRestart={handleReset} />
-          <ButtonGroup className='text-green-600 max-w-sm'>
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-          </ButtonGroup>
-          <Button className='bg-green-600 text-white p-2 flex justify-center max-w-sm m-4 rounded-lg hover:bg-green-700'>
-            Start Draft
-          </Button>
+        <div className='flex justify-between items-center w-full mt-4 border border-gray-300 rounded-lg'>
+          <div className='flex items-center'>
+            <h1 className='mr-4 p-2'>Rounds</h1>
+            <ButtonGroup className='text-green-600'>
+              <Button>1</Button>
+              <Button>2</Button>
+              <Button>3</Button>
+            </ButtonGroup>
+          </div>
+
+          <div className='flex justify-center items-center'>
+            <ResetDraftButton onRestart={handleReset} />
+            <Button className='bg-green-600 text-white p-2 flex justify-center max-w-sm m-4 rounded-lg hover:bg-green-700'>
+              Start Draft
+            </Button>
+          </div>
         </div>
       </div>
     </div>
