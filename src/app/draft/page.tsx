60159ba966737cb9d9015e6183508b2cc734058a
alimomennasab <alimomennasab3@gmail.com>
2024-07-31 13:36:56 -0700

    added draft page

---
+++ src/app/draft/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Prospect from "../../components/Prospect";
+
+export default function Page() {
+    return <p>Draft Page</p>;
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import { Button, ButtonGroup } from '@mui/material';
 import DraftOrderGrid, { TeamData, DraggableTeam } from '../components/DraftOrderGrid';
 import ResetDraftButton from '../components/ResetDraftButton';
 import SetRoundButtonGroup from '../components/SetRoundButtonGroup'; 
+import Prospect from '../components/Prospect';
 
 export default function Page() {
   const [draftCapital, setDraftCapital] = useState<DraggableTeam[]>([]);
   const [initialDraftCapital, setInitialDraftCapital] = useState<DraggableTeam[]>([]);
   const [selectedRound, setSelectedRound] = useState(1); 
+  const [draftStarted, setDraftStarted] = useState(false); 
 
   useEffect(() => {
     const fetchData = async () => {
@@ -42,37 +44,52 @@ export default function Page() {
   };
 
   const handleRoundChange = (round: number) => {
-    setSelectedRound(round); // Update selected round
+    setSelectedRound(round); 
+  };
+
+  const startDraft = () => {
+    setDraftStarted(true); 
   };
 
   return (
     <div className='flex justify-center items-center h-screen flex-col'>
-      <div className='border border-gray-200 rounded-lg p-6 shadow-lg w-full h-full md:w-auto'>
-        <div className='md:max-w-5xl flex flex-col items-center w-full'>
-          <div className='flex justify-between items-center w-full p-4'>
-            <div className='font-bold text-4xl'>
-              NFL 2025 Mock Draft
+      {!draftStarted ? (
+        <div className='border border-gray-200 rounded-lg p-6 shadow-lg w-full h-full md:w-auto'>
+          <div className='md:max-w-5xl flex flex-col items-center w-full'>
+            <div className='flex justify-between items-center w-full p-4'>
+              <div className='font-bold text-4xl'>
+                NFL 2025 Mock Draft
+              </div>
+              <SetRoundButtonGroup defaultRound={1} onRoundChange={handleRoundChange} />
             </div>
-            <SetRoundButtonGroup defaultRound={1} onRoundChange={handleRoundChange} />
-        </div>
 
-          <div className='md:max-w-5xl justify-center border border-gray-300 items-center p-4 rounded-lg w-full'>
-            <DraftOrderGrid draftCapital={draftCapital} setDraftCapital={setDraftCapital} />
-          </div>
-
-          <div className='flex flex-col items-center mt-3 w-full'>
-            <div className='italic text-xs text-gray-500 w-full text-center'>
-              Modify the draft order by dragging and dropping each team
+            <div className='md:max-w-5xl justify-center border border-gray-300 items-center p-4 rounded-lg w-full'>
+              <DraftOrderGrid draftCapital={draftCapital} setDraftCapital={setDraftCapital} />
             </div>
-            <div className='flex justify-center items-center'>
-              <ResetDraftButton onRestart={handleReset} />
-              <Button className='bg-green-600 text-white p-3 flex justify-center max-w-sm m-3 rounded-lg hover:bg-green-700'>
-                Start Draft
-              </Button>
+
+            <div className='flex flex-col items-center mt-3 w-full'>
+              <div className='italic text-xs text-gray-500 w-full text-center'>
+                Modify the draft order by dragging and dropping a team
+              </div>
+              <div className='flex justify-center items-center'>
+                <ResetDraftButton onRestart={handleReset} />
+                <Button 
+                  className='bg-green-600 text-white p-3 flex justify-center max-w-sm m-3 rounded-lg hover:bg-green-700'
+                  onClick={startDraft} // Add onClick handler to start the draft
+                >
+                  Start Draft
+                </Button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className='border border-gray-200 rounded-lg p-6 shadow-lg w-full h-full md:w-auto'>
+          <h1>Draft Page</h1>
+          <p>Round: {selectedRound}</p>
+          <Prospect rank={1} prospectName={'Luther Burden'} position={'WR'} logoURL={''}></Prospect>
+        </div>
+      )}
     </div>
   );
 }
