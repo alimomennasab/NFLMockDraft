889ce9e2ea51c72017f1ec8eafb5b10ffe70bafe
alimomennasab <alimomennasab3@gmail.com>
2024-08-08 17:41:14 -0700

    vercel deployment fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    postcss: true,
+  };
+  
+  export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
-import '../styles.css';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 
-export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    )
-  }
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <AppRouterCacheProvider>
+          {children}
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
